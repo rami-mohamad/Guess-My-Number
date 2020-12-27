1a423b5fa560ff3ede5c3502033d5826872306ab
rami mohamad <rami_zezo1986@hotmail.com>
2020-12-27 12:24:46 +0100

    the game is finish but need a littel bit style

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,48 @@
 'use strict';
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let message = document.querySelector('.message');
+let score = 20;
+let highscore = 0;
+
+let check = document.querySelector('.check').addEventListener('click', () => {
+  let data = Number(document.querySelector('.guess').value);
+  if (score > 1) {
+    if (!data || data < 1 || data > 20) {
+      message.textContent = 'its not between 1 and 20';
+    } else if (data > secretNumber) {
+      message.textContent = 'its too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (data < secretNumber) {
+      message.textContent = 'its too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+      message.textContent = 'congragulation !!!!';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('body').style.backgroundColor = 'graddeen';
+      message.style.color = 'yellow';
+      score--;
+    }
+  } else {
+    message.textContent = 'game over';
+    message.style.color = 'red';
+  }
+});
+// to reset every thing without highscore
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  message.textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  message.style.color = 'white';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = 'black';
+});
